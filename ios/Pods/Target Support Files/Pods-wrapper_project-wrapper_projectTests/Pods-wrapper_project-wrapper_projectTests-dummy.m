f6318e2f0aaf561773599046edbec3d6709e5a47
Tuan <tuanlt1989@gmail.com>
2021-08-03 14:40:35 +0700

    Add source

--- ios/Pods/Target Support Files/Pods-wrapper_project-wrapper_projectTests/Pods-wrapper_project-wrapper_projectTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_wrapper_project_wrapper_projectTests : NSObject
-@end
-@implementation PodsDummy_Pods_wrapper_project_wrapper_projectTests
-@end
--- ios/Pods/Target Support Files/Pods-wrapper_project/Pods-wrapper_project-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_wrapper_project : NSObject
-@end
-@implementation PodsDummy_Pods_wrapper_project
-@end
